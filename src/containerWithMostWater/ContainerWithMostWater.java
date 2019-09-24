c19064aa602d6bb7b80e5df1ad1c3c4d37161446
las <li_as1991@163.com>
2019-09-24 16:57:22 +0800

    ContainerWithMostWater

--- src/containerWithMostWater/ContainerWithMostWater.java
+++ src/containerWithMostWater/ContainerWithMostWater.java
@@ -2,8 +2,17 @@ package containerWithMostWater;
 
 public class ContainerWithMostWater {
     public int maxArea(int[] height) {
-
-        return 0;
+        int m = 0, n = height.length - 1;
+        int max = 0;
+        while (m < n) {
+            int h = height[m] < height[n] ? height[m] : height[n];
+            if (h * (n - m) > max) {
+                max = h * (n - m);
+            }
+            if (height[m] <= height[n]) m++;
+            else n--;
+        }
+        return max;
     }
 
 }
