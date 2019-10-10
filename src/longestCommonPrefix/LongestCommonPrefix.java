2f37b57fc248a996c405a4720bf2b9b81b8443c6
las <li_as1991@163.com>
2019-10-10 13:26:23 +0800

    LongestCommonPrefix

--- src/longestCommonPrefix/LongestCommonPrefix.java
+++ src/longestCommonPrefix/LongestCommonPrefix.java
@@ -14,8 +14,9 @@ public class LongestCommonPrefix {
         }
         String tmp = strs[0];
         for (int i = 1; i < strs.length; i++) {
-            while (strs[i].indexOf(tmp) != 0 && tmp.length() > 0) {
+            while (strs[i].indexOf(tmp) != 0) {
                 tmp = tmp.substring(0, tmp.length() - 1);
+                if (tmp.isEmpty()) return tmp;
             }
         }
         return tmp;
